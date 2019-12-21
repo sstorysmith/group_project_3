5a941f9e570f29f88899539dcfbdcd87dee95ae5
Thomas Mylott <mylottiv@gmail.com>
2019-12-20 18:40:36 -0600

    Better string visualizations of spatial relationship

--- tournament-game/src/App.js
+++ tournament-game/src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
 function draw(size) {
-  if (size === 0) {
-    return `Size=${size} Name \n`
+  if (size === 1) {
+    let indent = ''
+    for (let i = 0; i < size; i++) {
+      // indent+= 't'
+      indent+= '\u00A0';
+    }
+    let indent2 = indent.substring(0, size - 1);
+    return <span><br />{indent}Undecided size={size}, <br />Name {indent2}VS Name</span>
   }
   else {
-    return <span><br />Parent size={size}, <br />{draw(size-1)}-{draw(size-1)}</span>
+    let indent = ''
+    for (let i = 0; i < size; i++) {
+      // indent+= 't'
+      indent+= '\u00A0';
+    }
+    let indent2 = indent.substring(0, size - 1);
+    console.log(indent2)
+    return <span><br />{indent}Undecided size={size}, (<br />{draw(size-1)}<br /> {indent2}VS {draw(size-1)})</span>
   }
 }
 
 function App() {
   return (
     <div>
-      <p>{draw(4)}</p>
+      {draw(3)}
     </div>
   );
 }

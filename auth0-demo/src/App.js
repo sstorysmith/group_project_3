a2c6ddf63dd12151cfd0e4b887c411ccfdaec141
reducks3000 <reducks3000@gmail.com>
2020-01-05 01:08:42 -0600

    imported demo files to folder

--- auth0-demo/src/App.js
+++ auth0-demo/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Main from "./components/Main";
+import Secret from "./components/Secret";
+import NotFound from "./components/NotFound";
+import Callback from "./components/Callback";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+      let mainComponent = ""
+      switch(this.props.location) {
+        case "":
+          mainComponent = <Main {...this.props}/>;
+          break;
+        case "callback":
+          mainComponent = <Callback />;
+          break;
+        case "secret":
+          mainComponent = this.props.auth.isAuthenticated() ? <Secret {...this.props} /> : <NotFound/>;
+          break;
+        default:
+          mainComponent= <NotFound />;
+      }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React, {this.props.name}</h1>
+        </header>
+          {mainComponent}
+      </div>
+    );
+  }
 }
 
 export default App;

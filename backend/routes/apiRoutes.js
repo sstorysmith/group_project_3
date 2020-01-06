5b21fe099c210959f9c5d3184ce5cdc6375bf277
Thomas Mylott <mylottiv@gmail.com>
2020-01-06 17:57:35 -0600

    Started outlining potential backend routes

---
+++ backend/routes/apiRoutes.js
@@ -0,0 +1,31 @@
+// GET Routes
+
+    // GET Open Tournaments (optional query)
+
+    // GET Archived Tournaments (optional query)
+
+    // GET Players Stats (optional query)
+
+    // GET Open Tournament (id)
+
+    // GET Archived Tournament (id)
+
+    // GET Player Stats (id)
+
+// POST Routes
+
+    // POST New Player Stat (id)
+
+    // POST New Tournament
+
+    // POST New Player
+
+// PATCH Routes
+
+    // PATCH Open Tournament (id)
+
+    // PATCH Player Stats (id)
+
+// DELETE Routes
+
+    // DELETE Player

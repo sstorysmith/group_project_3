366fa9a96e613454bf2f61b6dae7e75b2add61e1
reducks3000 <reducks3000@gmail.com>
2020-01-07 18:30:08 -0600

    added paths and pages to routing of app

--- tournament-app/src/App.js
+++ tournament-app/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Nav from './Nav';
+import About from './components/About';
+import Profile from './components/Profile';
+import Tournament from './components/Tournament'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/about" exact component={About} />
+          <Route path="/profile" exact component={Profile} />
+          <Route path="/tournament" exact component={Tournament} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const HomePage = () => (
+  <div>
+    <h1>Home Page!</h1>
+  </div>
+)
+
 
 export default App;
---
+++ tournament-app/src/Nav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+
+function Nav() {
+
+    const navStyle = {
+        color: 'white'
+    }
+    return (
+        <nav>
+            <h3>logo</h3>
+            <ul className="nav-links">
+                <Link style={navStyle} to='/about'>
+                    <il>About</il>
+                </Link>
+                <Link style={navStyle} to='/Tournament'>
+                    <il>Tournament</il>
+                </Link>
+                <Link style={navStyle} to='/profile'>
+                    <il>Profile</il>
+                </Link>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;

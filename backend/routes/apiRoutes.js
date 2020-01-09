d01a5abc454d3364889cbc42cc1b24a11cef11de
Thomas Mylott <mylottiv@gmail.com>
2020-01-09 14:53:31 -0600

    Pseudocoding API data routes

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -2,19 +2,47 @@
 
     // GET Open Tournaments (optional query)
 
+        // Return a list of all tournaments waiting for more players
+
+        // Constrained by a query, if provided
+
     // GET Archived Tournaments (optional query)
 
-    // GET Players Stats (optional query)
+        // Return a list of all tournaments that have finished
+
+        // Constrained by a query, if provided
+
+    // GET Active Tournaments (optional query)
+
+        // Return a list of all ongoing tournaments
+
+        // Constrained by a query, if provided
+
+    // GET Players Stats (player query, stat query)
+
+        // Return a list of all stats for all players constrained by a query
 
     // GET Open Tournament (id)
 
+        // Return the open tournament for a given id
+
     // GET Archived Tournament (id)
 
-    // GET Player Stats (id)
+        // Return the closed tournament for a given id
+
+    // GET Active Tournament (id)
+    
+        // Return the active tournament for a given id
+
+    // GET Player Stats (id, query)
+
+        // Return player stats constrained by query for a given id
 
 // POST Routes
 
-    // POST New Player Stat (id)
+    // POST New Player Game Category (id)
+
+        // 
 
     // POST New Tournament
 
